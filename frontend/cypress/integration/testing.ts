3aacb9cf9910df50456e0721a972e5372e34445a
Tamara Jordan <tamara.jordan1+coding@hotmail.com>
2022-01-08 22:05:07 +0000

    Be able to enter player names

--- frontend/cypress/integration/testing.ts
+++ frontend/cypress/integration/testing.ts
@@ -6,6 +6,12 @@ describe('Can play a 2 player game', () => {
     cy.title().should('equal', 'Skull King Calculator')
     cy.findByText('New game').click()
     cy.findByText('Game setup').should('be.visible')
+
+    // TODO: Feature: remove a player
+    cy.findByText('Add player').click()
+    cy.findByPlaceholderText('Player 1').type('Tam')
+    cy.findByText('Add player').click()
+    cy.findByPlaceholderText('Player 2').type('Peter')
   })
 })
 
--- frontend/src/components/welcome/GameSetup.tsx
+++ frontend/src/components/welcome/GameSetup.tsx
@@ -1,3 +1,27 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+import { WithClassName } from '~/styles'
+
+type PlayerInfo = { name?: string }
+
 export const GameSetup: React.FC = () => {
-  return <h1>Game setup</h1>
+  const [players, setPlayers] = useState<PlayerInfo[]>([])
+
+  return (
+    <section>
+      <h1>Game setup</h1>
+      <button onClick={() => setPlayers([...players, {}])}>Add player</button>
+      {players.map((p, i) => (
+        <PlayerInput key={i} index={i} />
+      ))}
+    </section>
+  )
 }
+
+type PlayerInputProps = WithClassName & PlayerInfo & { index: number }
+
+const PlayerInput = styled(({ className, index }: PlayerInputProps) => {
+  return <input className={className} type="text" placeholder={`Player ${index + 1}`} />
+})`
+  display: block;
+`
