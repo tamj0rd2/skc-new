95c4edd97fe6eff79698201e878dd9fbb03c5f60
Tamara Jordan <tamara.jordan1+coding@hotmail.com>
2022-01-09 09:53:17 +0000

    Be able to select the winner of a trick

--- frontend/src/components/PlayingGame.tsx
+++ frontend/src/components/PlayingGame.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import styled from 'styled-components'
 import { Game, Player, RoundPhase, StartRoundAction, WithDispatch } from '~/domain/state'
 
 interface PlayingGameProps extends WithDispatch {
@@ -20,7 +22,7 @@ export const PlayingGame: React.FC<PlayingGameProps> = ({ game, dispatch }) => {
 const RoundSetup: React.FC<PlayingGameProps & { playerToDeal: Player }> = (props) => {
   const { game, dispatch, playerToDeal } = props
   return (
-    <section>
+    <div>
       <h1>
         Round {game.roundIndex + 1}: {playerToDeal.name} deals, then place bets
       </h1>
@@ -33,17 +35,56 @@ const RoundSetup: React.FC<PlayingGameProps & { playerToDeal: Player }> = (props
         ))}
       </ul>
       <button onClick={() => dispatch(new StartRoundAction([]))}>Start round</button>
-    </section>
+    </div>
   )
 }
 
 const RoundPlaying: React.FC<PlayingGameProps & { playerToStart: Player }> = (props) => {
   const { game, playerToStart } = props
+  const numOfTricks = game.roundIndex + 1
+
   return (
-    <section>
+    <div>
       <h1>
         Round {game.roundIndex + 1}: Play cards - {playerToStart.name} starts
       </h1>
-    </section>
+      {new Array(numOfTricks).fill(0).map((_, i) => (
+        <TrickScoringBit key={i} trickIndex={i} players={game.players} />
+      ))}
+    </div>
   )
 }
+
+const TrickScoringBit: React.FC<{ trickIndex: number; players: Player[] }> = (props) => {
+  const { trickIndex, players } = props
+  const [winner, setWinner] = useState<Player | undefined>()
+
+  return (
+    <TrickScoring>
+      <h2>Trick {trickIndex + 1} winner:</h2>
+      <ul>
+        {players.map((player, i) => (
+          <WinnerButton key={i} isWinner={winner === player} onClick={() => setWinner(player)}>
+            {player.name}
+          </WinnerButton>
+        ))}
+      </ul>
+    </TrickScoring>
+  )
+}
+
+const TrickScoring = styled.section`
+  h2 {
+    display: inline-block;
+  }
+
+  ul {
+    display: inline-block;
+    padding-left: 0;
+  }
+`
+
+const WinnerButton = styled.button<{ isWinner?: boolean }>`
+  background: ${(props) => (props.isWinner ? 'green' : 'default')};
+  color: ${(props) => (props.isWinner ? 'white' : 'default')};
+`
